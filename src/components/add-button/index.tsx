7e6d8e60d7c1ece3db2ca3ec260ce8ef79b3b954
Mateo Mazzucco <mmazzucco@Mateos-Air.fibertel.com.ar>
2024-10-13 13:44:08 -0300

    feat: Adding forms to create products and prices

--- src/components/add-button/index.tsx
+++ src/components/add-button/index.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react'
+import { FormModal } from '../form-modal';
 
 export const AddButton: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [shouldAddPrice, setShouldAddPrice] = useState(false);
+  const [shouldAddProduct, setShouldAddProduct] = useState(false);
+
+  const handleAddPriceButton = () => {
+    setShouldAddPrice(true);
+  }
+
+  const handleAddProductButton = () => {
+    setShouldAddProduct(true);
+  }
 
   return (
     <div className="add-button-container">
@@ -13,10 +24,12 @@ export const AddButton: React.FC = () => {
       </button>
       {isOpen && (
         <div className="popup">
-          <button onClick={() => console.log('Create x clicked')} style={{ fontWeight: 'bold' }}>AGREGAR PRECIO</button>
-          <button onClick={() => console.log('Create y clicked')} style={{ fontWeight: 'bold' }}>CREAR PRODUCTO</button>
+          <button onClick={handleAddPriceButton} style={{ fontWeight: 'bold' }}>AGREGAR PRECIO</button>
+          <button onClick={handleAddProductButton} style={{ fontWeight: 'bold' }}>AGREGAR PRODUCTO</button>
         </div>
       )}
+      <FormModal isOpen={shouldAddPrice} setIsOpen={setShouldAddPrice} setAddButton={setIsOpen} title='Nuevo precio' buttonName='Agregar precio' type='price' />
+      <FormModal isOpen={shouldAddProduct} setIsOpen={setShouldAddProduct} setAddButton={setIsOpen} title='Nuevo producto' buttonName='Agregar producto' type='product' />
     </div>
   )
-}
+} 
--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -1,25 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 
 interface ProductProps {
   imageUrl: string;
   price: number;
   location: string;
+  name: string;
 }
 
-export const ProductCard: React.FC<ProductProps> = ({ imageUrl, price, location }) => {
+export const ProductCard: React.FC<ProductProps> = ({ imageUrl, price, location, name }) => {
   return (
     <div style={{ 
       borderRadius: '18px', 
       padding: '10px', 
       marginBottom: '20px',
       maxWidth: '300px',
+      height: '360px',
       boxShadow: '2px 4px 12px #00000014',
       transition: 'all .3s cubic-bezier(0,0,.5,1)',
       whiteSpace: 'normal',
-      backgroundColor: '#fff'
+      backgroundColor: '#fff',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between'
     }}>
-      <div style={{ marginBottom: '10px', width: '100%', display: 'flex', justifyContent: 'center', alignContent: 'center', fontWeight: 'bold' }}>COCA COLA</div>
-      <img src={imageUrl} alt="Product" style={{ width: '100%', height: 'auto' }} />
+      <div style={{ marginBottom: '10px', width: '100%', display: 'flex', justifyContent: 'center', alignContent: 'center', fontWeight: 'bold' }}>{name}</div>
+      <img src={imageUrl} alt="Product" style={{ 
+        width: '100%', 
+        height: '250px',
+        objectFit: 'contain',
+        objectPosition: 'center'
+      }} />
       <div style={{ marginTop: '10px', display: 'flex', justifyContent: 'space-between' }}>
         <p style={{ color: '#1d1d1f', fontWeight: 'bold' }}>
           💲 {price}
